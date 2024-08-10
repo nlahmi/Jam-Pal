b7f11f99e64b974942bd80f4b7a91a58f2cdb789
n.lahmi <mogobekl123@gmail.com>
2024-08-10 14:10:25 +0300

    Added project creation

--- main.py
+++ main.py
@@ -1,17 +1,61 @@
+from pathlib import Path
+import os
+import shutil
+import subprocess
+from time import sleep
+
 import demucs.separate
-from reathon.nodes import Project, Node, Path, Track, Item
+import reapy
+
 # import shlex
 
-model = "mkx_extra"
-temp_base_path = "separated"
-file_path = "test.mp3"
+# snake_case - author-song_name
+project_name = "author-test"
+template_file = Path("project_template.RPP")
+
+
+# Create new project
+def create_project():
+    recordings_base_path = Path("/home/noam/projects/Recordings/")
+    project_base_path = recordings_base_path / project_name
+    project_file = project_base_path / f"{project_name}.RPP"
+
+    # If dir already exists, also don't copy the template file
+    try:
+        os.mkdir(project_base_path)
+        shutil.copyfile(template_file, project_file)
+    except FileExistsError:
+        print("Dir already exists")
+
+    # Open reaper with out project in a detached process
+    subprocess.Popen(["reaper", project_file], start_new_session=True)
+    # sleep(10)
+
 
 # Separate tracks
-# demucs.separate.main(["--mp3", "-n", model, file_path])
+def separate_tracks():
+    model = "mkx_extra"
+    temp_base_path = "separated"
+    file_path = "test.mp3"
+    demucs.separate.main(["--mp3", "-n", model, file_path])
 
-project = Project(Track(), Track(), Track())
-project.write("test.rpp")
 
+def reapy_test():
+    project = reapy.Project()
+    # reapy.print("yay")
+
+
+def init():
+    reapy.configure_reaper()
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    init()
+    main()
 
 print("done!")
 
@@ -22,5 +66,7 @@ print("done!")
 # Create a Reaper project with reathon and include the files and tempo
 # Start it and.. profit(?)
 
-# Extra: Do a bass2midi detection
+# Extra1: Do a bass2midi detection
 # Extra2: Do a midi2tabs conversion
+# Extra3: Chords Overlay (markers?)
+# Extra4: Lyrics
